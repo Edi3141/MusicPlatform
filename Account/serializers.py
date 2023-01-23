997b71fb34fa447539c6e803c298b7c87b83d1dd
Kydyrbek97 <Kydyrali_097@mail.ru>
2023-01-23 19:57:09 +0600

    added Account

---
+++ Account/serializers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=8, max_length=20, required=True, write_only=True)
+    password2 = serializers.CharField(min_length=8, max_length=20, required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'password2', 'last_name', 'first_name', 'username', 'avatar')
+
+    def validate(self, attrs):
+        password2 = attrs.pop('password2')
+        if attrs['password'] != password2:
+            raise serializers.ValidationError('Пароли не совпадают!')
+        if not attrs['password'].isalnum():
+            raise serializers.ValidationError('Поле пароля должно содержать буквенные и цифровые символы!')
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    default_error_messages = {'bad_token': _('Токен не действителен или просрочен!')}
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail('bad_token')
+
